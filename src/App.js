48ac120e0c2c8a7cf356d665ebf5e3e11fd35665
Mheyrie <omota4fidelity@gmail.com>
2024-08-01 00:26:01 +0100

    Created a separate file for recommended

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navigation/Navbar";
+import Products from "./products/Products";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Products/>
     </>
   );
 }
---
+++ src/recommended/Recommended.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/recommended.Recommended'
+
+function Recommended() {
+  return (
+    <div>Recommended</div>
+  )
+}
+
+export default Recommended
