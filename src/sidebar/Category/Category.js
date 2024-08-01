ab1d08404b247cd348112972a33f7d9f7d962c5c
Mheyrie <omota4fidelity@gmail.com>
2024-08-01 14:50:59 +0100

    Category styling

--- src/sidebar/Category/Category.js
+++ src/sidebar/Category/Category.js
@@ -4,11 +4,27 @@ import '../../styles/sidebar/Category.css'
 function Category() {
   return (
     <div>
-      <h2 className="sidebar-title">Category</h2>
+      <h2 className="category-title">Category</h2>
       <div>
-        <label htmlFor="" className="sidebar-label-container">
+        <label className="category-label-container">
           <input type="radio" name="test" />
           <span className="checkmark"></span> All
+        </label>
+        <label className="category-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span> Sneakers
+        </label>
+        <label className="category-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span> Flats
+        </label>
+        <label className="category-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span> Heels
+        </label>
+        <label className="category-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span> Sandals
         </label>
       </div>
     </div>
