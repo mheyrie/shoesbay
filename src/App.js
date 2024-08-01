12906030b05889ef97d8586757e10a7f03058387
Mheyrie <omota4fidelity@gmail.com>
2024-08-01 01:00:27 +0100

    Working on navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./Navigation/Navbar";
 import Products from "./products/Products";
+import Recommended from "./recommended/Recommended";
+import "./index.css"
 
 function App() {
   return (
     <>
       <Navbar />
       <Products/>
+      <Recommended/>
     </>
   );
 }
--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import '../styles/navigation/Navbar.css'
+import React from "react";
+import "../styles/navigation/Navbar.css";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd} from "react-icons/ai";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <div className="nv-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your shoes name to search"
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons"/>
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons"/>
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons"/>
+        </a>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
