654712aaf772753968b49c6d205abd164611b8cf
Mheyrie <omota4fidelity@gmail.com>
2024-08-02 10:27:12 +0100

    Debugging

--- src/sidebar/Colors/Colors.js
+++ src/sidebar/Colors/Colors.js
@@ -6,29 +6,29 @@ function Colors({ handleChange }) {
     <div>
       <h2 className="sidebar-title color-title">Color</h2>
       <label className="sidebar-label-container">
-        <input type="radio" onChange={handleChange} value="" name="test" />
+        <input type="radio" onChange={handleChange} value="" name="test3" />
         <span className="checkmark all"></span>All
       </label>
       <Input
         handleChange={handleChange}
         value="black"
         title="Black"
-        name="test1"
+        name="test3"
         color="black"
       />
-      <Input handleChange={handleChange} value="red" title="Red" name="test1"color="red" />
+      <Input handleChange={handleChange} value="red" title="Red" name="test3"color="red" />
       <Input
         handleChange={handleChange}
         value="green"
         title="Green"
-        name="test1"
+        name="test3"
         color="green"
       />
       <Input
         handleChange={handleChange}
         value="blue"
         title="Blue"
-        name="test1"
+        name="test3"
         color="blue"
       />
       <label className="sidebar-label-container">
