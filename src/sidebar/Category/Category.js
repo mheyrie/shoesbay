cef3264481bb89d1b909fecab49475459bf28053
Mheyrie <omota4fidelity@gmail.com>
2024-08-02 12:03:05 +0100

    Updated

--- src/sidebar/Category/Category.js
+++ src/sidebar/Category/Category.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import '../../styles/sidebar/Category.css'
-import Input from "../../components/Input"
+import React from "react";
+import "../../styles/sidebar/Category.css";
+import Input from "../../components/Input";
 
-function Category({handleChange}) {
+function Category({ handleChange }) {
   return (
     <div>
       <h2 className="sidebar-title category-title">Category</h2>
       <div>
         <label className="sidebar-label-container">
-          <input type="radio" onChange={handleChange} value=""  name="test"/>
+          <input type="radio" onChange={handleChange} value="" name="test" />
           <span className="checkmark"></span>All
         </label>
 
         <Input
-        handleChange={handleChange}
-        value="sneakers"
-        title="Sneakers"
-        name="test"
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="test"
         />
         <Input
-        handleChange={handleChange}
-        value="flats"
-        title="Flats"
-        name="test"
+          handleChange={handleChange}
+          value="flats"
+          title="Flats"
+          name="test"
         />
         <Input
-        handleChange={handleChange}
-        value="sandals"
-        title="Sandals"
-        name="test"
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="test"
         />
         <Input
-        handleChange={handleChange}
-        value="heels"
-        title="Heels"
-        name="test"
+          handleChange={handleChange}
+          value="heels"
+          title="Heels"
+          name="test"
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
