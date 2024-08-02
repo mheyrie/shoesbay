960fc9701b8380aecca26c9697c03cd22a45af21
Mheyrie <omota4fidelity@gmail.com>
2024-08-02 09:10:43 +0100

    Dymanic rendering of products

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,21 +8,21 @@ function Card({ img, title, star, reviews, prevPrice, newPrice }) {
     <section className="product">
         <img
           src={img}
-          alt="shoes"
+          alt={title}
           className="product-img"
         />
         <div className="product-details">
-          <h3 className="product-title">Shoes</h3>
+          <h3 className="product-title">{title}</h3>
           <section className="product-review">
-            <AiFillStar className="rating-star" />
-            <AiFillStar className="rating-star" />
-            <AiFillStar className="rating-star" />
-            <AiFillStar className="rating-star" />
-            <span className="total-reviews">4</span>
+            {star}
+            {star}
+            {star}
+            {star}
+            <span className="total-reviews">{reviews}</span>
           </section>
           <section className="product-price">
             <div className="price">
-              <del>$300</del> $200
+              <del>{prevPrice}</del> ${newPrice}
             </div>
             <div className="bag">
               <BsFillBagHeartFill className="bag-icon"/>
